89aa260d5f0fbe0d61259965f7e991256623923d
Pradyut Das <77787607+prxdyut@users.noreply.github.com>
2024-03-02 18:32:55 +0530

    Basic File Uploading using S3 done!

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "https://simplefilesharing69.s3.ap-south-1.amazonaws.com/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
